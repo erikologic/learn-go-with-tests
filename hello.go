9e10bba35f5930d17ec30cd841ae6dd148fd14aa
Enrico Graziani <mrenrich84@gmail.com>
2022-08-14 23:44:14 +0100

    accepting multiple languages

--- hello.go
+++ hello.go
@@ -4,13 +4,32 @@ import "fmt"
 
 const englishHelloPrefix = "Hello, "
 
-func Hello(name string) string {
+const spanish = "Spanish"
+const spanishHelloPrefix = "Hola, "
+
+const french = "French"
+const frenchHelloPrefix = "Bonjour, "
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
 }
 
 func main() {
-	fmt.Println(Hello("you"))
+	fmt.Println(Hello("you", ""))
 }
