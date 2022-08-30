faa29beb75be56793b9806cde6b34258437eedb7
Enrico Graziani <mrenrich84@gmail.com>
2022-08-30 18:11:23 +0100

    support empty files

--- playerServer/file_system_store_test.go
+++ playerServer/file_system_store_test.go
@@ -12,7 +12,8 @@ func TestFileSystemStore(t *testing.T) {
             {"Name": "Chris", "Wins": 33}]`)
 		defer cleanDatabase()
 
-		store := NewFileSystemPlayerStore(database)
+		store, err := NewFileSystemPlayerStore(database)
+		assertNoError(t, err)
 
 		got := store.GetLeague()
 
@@ -33,7 +34,8 @@ func TestFileSystemStore(t *testing.T) {
             {"Name": "Chris", "Wins": 33}]`)
 		defer cleanDatabase()
 	
-		store := NewFileSystemPlayerStore(database)
+		store, err := NewFileSystemPlayerStore(database)
+		assertNoError(t, err)
 	
 		got := store.GetPlayerScore("Chris")
 		assertScoreEquals(t, got, 33)
@@ -45,8 +47,9 @@ func TestFileSystemStore(t *testing.T) {
 			{"Name": "Chris", "Wins": 33}]`)
 		defer cleanDatabase()
 	
-		store := NewFileSystemPlayerStore(database)
-	
+		store, err := NewFileSystemPlayerStore(database)
+		assertNoError(t, err)
+			
 		store.RecordWin("Chris")
 	
 		got := store.GetPlayerScore("Chris")
@@ -60,13 +63,23 @@ func TestFileSystemStore(t *testing.T) {
 			{"Name": "Chris", "Wins": 33}]`)
 		defer cleanDatabase()
 	
-		store := NewFileSystemPlayerStore(database)
-	
+		store, err := NewFileSystemPlayerStore(database)
+		assertNoError(t, err)
+
 		store.RecordWin("Pepper")
 	
 		got := store.GetPlayerScore("Pepper")
 		want := 1
 		assertScoreEquals(t, got, want)
+	})
+
+	t.Run("works with an empty file", func(t *testing.T) {
+		database, cleanDatabase := createTempFile(t, "")
+		defer cleanDatabase()
+	
+		_, err := NewFileSystemPlayerStore(database)
+	
+		assertNoError(t, err)
 	})
 }
 
@@ -93,5 +106,12 @@ func assertScoreEquals(t testing.TB, got, want int) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %d want %d", got, want)
+	}
+}
+ 
+func assertNoError(t testing.TB, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatalf("didn't expect an error but got one, %v", err)
 	}
 }
